a36146714521fa91c1d7c38274bb2801e0e38546
celine.denis <celine.denis29@yahoo.fr>
2026-03-23 11:44:04 +0100

    Ajout des dependances

--- src/main/java/com/accenture/projetapipizza/repository/CustomerDao.java
+++ src/main/java/com/accenture/projetapipizza/repository/CustomerDao.java
@@ -1,4 +1,7 @@
 package com.accenture.projetapipizza.repository;
 
-public interface CustomerDao {
+import com.accenture.projetapipizza.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerDao extends JpaRepository<Customer, Integer> {
 }
