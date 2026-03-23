382a335c3331c79359cbd86a9c749aeb71bff675
celine.denis <celine.denis29@yahoo.fr>
2026-03-23 09:47:00 +0100

    First commit

---
+++ src/test/java/com/accenture/projetapipizza/ProjetApiPizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.projetapipizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetApiPizzaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
